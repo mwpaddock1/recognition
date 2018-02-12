e60ce1906e0910cb70d2f8a0992c0711c8a5c665
Marjorie Paddock <marjorie.paddock@gmail.com>
2018-02-12 16:31:47 -0500

    adding  'module.export'

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.static('public'));
+// app.listen(process.env.PORT || 8080, () => {
+//     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+//   });;
+'use strict';
+
 const express = require('express');
+
 const app = express();
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-  });;
+
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function () {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
+
+module.exports = app;
