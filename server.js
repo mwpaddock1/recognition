64ae5c22f5ad185f49d3cf3d8f030932d34dee3e
Marjorie Paddock <marjorie.paddock@gmail.com>
2018-02-12 17:12:52 -0500

    adding Morgan

--- server.js
+++ server.js
@@ -7,8 +7,11 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('common'));
+// app.use(morgan(':date[iso] :method :url :response-time'));
 
 app.use(express.static('public'));
 
