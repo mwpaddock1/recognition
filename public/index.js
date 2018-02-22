f0983fc9432ae6a6c01f8dc5e6f8ad6ac28d66b3
Marjorie Paddock <marjorie.paddock@gmail.com>
2018-02-22 14:11:40 -0500

    removing button from goal ellipses

--- public/index.js
+++ public/index.js
@@ -37,37 +37,41 @@ const MOCK_DATA = {
             lastName: "Worker",
             firstName: "John",
             emailAddress: "jworker@fizzbuzz.com",
-            pointsGiven: 0,                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+            pointsGiven: 0,
             pointsReceived: 0,
             pointsRemaining: 100
         }
     ]
 };
-                                                                                   
+
 function getEmployees(callbackFn) {
-    setTimeout(function(){callbackFn(MOCK_DATA)}, 100);
+    setTimeout(function () {
+        callbackFn(MOCK_DATA)
+    }, 100);
 }
+
+
+
 
 // this function stays the same when we connect
 // to real API later
-function displayEmployees(data) {    
-    for (let currentEmployee =0; currentEmployee < data.employees.length; currentEmployee++) {
-       $('#js-last-name').append(`<h2>${data.employees[currentEmployee].lastName}</h2>`);        
-       $('#js-first-name').append(`<h2>${data.employees[currentEmployee].firstName}</h2>`);        
-       $('#js-points-received').append(`<h2>${data.employees[currentEmployee].pointsReceived}</h2>`);
-       $('#js-points-given').append(`<h2>${data.employees[currentEmployee].pointsGiven}</h2>`);   
-       $('#js-points-remaining').append(`<h2>${data.employees[currentEmployee].pointsRemaining}</h2>`);
-     
-    }
-}
+ function displayEmployees(data) {
+    for (let currentEmployee = 0; currentEmployee < data.employees.length; currentEmployee++) {
+      $('#js-last-name').append(`<h2>${data.employees[currentEmployee].lastName}</h2>`);
+      $('#js-first-name').append(`<h2>${data.employees[currentEmployee].firstName}</h2>`);
+      $('#js-points-received').append(`<h2>${data.employees[currentEmployee].pointsReceived}</h2>`);
+      $('#js-points-given').append(`<h2>${data.employees[currentEmployee].pointsGiven}</h2>`);
+      $('#js-points-remaining').append(`<h2>${data.employees[currentEmployee].pointsRemaining}</h2>`);
+
+     }
+ }
 
 // this function can stay the same even when we
 // are connecting to real API
- function getAndDisplayEmployees() {
-     getEmployees(displayEmployees);
- }
+function getAndDisplayEmployees() {
+    getEmployees(displayEmployees);
+}
 
- $(function() {
+$(function () {
     getAndDisplayEmployees();
- });
-
+});
