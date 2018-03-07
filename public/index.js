0026740a06fbbf81cff292dbc1ba80531bed13ae
Marjorie Paddock <marjorie.paddock@gmail.com>
2018-03-07 16:23:19 -0500

    Adding local storage

--- public/index.js
+++ public/index.js
@@ -1,61 +1,59 @@
-const MOCK_DATA = {
-    employees: [{
-            lastName: "Foo",
-            firstName: "Joseph",
-            emailAddress: "jfoo@fizzbuzz.com",
-            pointsGiven: 10,
-            pointsReceived: 5,
-            pointsRemaining: 90,
-            pointsFromLastName: "Bar",
-            pointsFromFirstName: "Mary",
-            corpGoal: "costs",
-            points: 5,
-            reason: "sourced a new paper vendor"
-        },
-        {
-            lastName: "Bar",
-            firstName: "Mary",
-            emailAddress: "mbar@fizzbuzz.com",
-            pointsGiven: 5,
-            pointsReceived: 0,
-            pointsRemaining: 95,
-            pointsFromLastName: "Staff",
-            pointsFromFirstName: "John",
-            corpGoal: "service",
-            points: 10,
-            reason: "served dinner at the homeless shelter"
-        },
-        {
-            lastName: "Staff",
-            firstName: "John",
-            emailAddress: "jstaff@fizzbuzz.com",
-            pointsGiven: 0,
-            pointsReceived: 10,
-            pointsRemaining: 100
-        },
-        {
-            lastName: "Worker",
-            firstName: "John",
-            emailAddress: "jworker@fizzbuzz.com",
-            pointsGiven: 0,
-            pointsReceived: 0,
-            pointsRemaining: 100
-        }
-    ]
-};
+// console.log(JSON.stringify({
+//     employees: [{
+//             lastName: "Foo",
+//             firstName: "Joseph",
+//             emailAddress: "jfoo@fizzbuzz.com",
+//             pointsGiven: 10,
+//             pointsReceived: 5,
+//             pointsRemaining: 90,
+//             pointsFromLastName: "Bar",
+//             pointsFromFirstName: "Mary",
+//             corpGoal: "costs",
+//             points: 5,
+//             reason: "sourced a new paper vendor"
+//         },
+//         {
+//             lastName: "Bar",
+//             firstName: "Mary",
+//             emailAddress: "mbar@fizzbuzz.com",
+//             pointsGiven: 5,
+//             pointsReceived: 0,
+//             pointsRemaining: 95,
+//             pointsFromLastName: "Staff",
+//             pointsFromFirstName: "John",
+//             corpGoal: "service",
+//             points: 10,
+//             reason: "served dinner at the homeless shelter"
+//         },
+//         {
+//             lastName: "Staff",
+//             firstName: "John",
+//             emailAddress: "jstaff@fizzbuzz.com",
+//             pointsGiven: 0,
+//             pointsReceived: 10,
+//             pointsRemaining: 100
+//         },
+//         {
+//             lastName: "Worker",
+//             firstName: "John",
+//             emailAddress: "jworker@fizzbuzz.com",
+//             pointsGiven: 0,
+//             pointsReceived: 0,
+//             pointsRemaining: 100
+//         }
+//     ]
+// }));
 
-function getEmployees(callbackFn) {
-    setTimeout(function () {
-        callbackFn(MOCK_DATA)
-    }, 100);
-}
+// function getEmployees(callbackFn) {
+//     setTimeout(function () {
+//         callbackFn(MOCK_DATA)
+//     }, 100);
+// }
 
 // this function stays the same when we connect
 // to real API later
 function displayEmployees(employees) {
-
     for (let currentEmployee = 0; currentEmployee < employees.length; currentEmployee++) {
-
         $('#js-last-name').append(`<h2>${employees[currentEmployee].lastName}</h2>`);
         $('#js-first-name').append(`<h2>${employees[currentEmployee].firstName}</h2>`);
         $('#js-points-received').append(`<h2>${employees[currentEmployee].pointsReceived}</h2>`);
@@ -69,7 +67,6 @@ $("form[name=sign-up-form]").submit(function (event) {
     //get the form inputs and place them into an array
     let inputArray =
         $(event.currentTarget).serializeArray();
-
     //reformat the array
     let reformattedArray = inputArray.map(item => {
         let rObj = {};
@@ -77,9 +74,6 @@ $("form[name=sign-up-form]").submit(function (event) {
         rObj[item.name] = item.value;
         return rObj;
     })
-
-    console.log(reformattedArray);
-
     const reducingFunction = (obj1, obj2) =>
         Object.assign(obj1, obj2);
     let newEmployee = (reformattedArray.reduce(reducingFunction));
@@ -92,15 +86,56 @@ $("form[name=sign-up-form]").submit(function (event) {
         .then(displayEmployees);
 });
 
+function getMockData() {
+    let MOCK_DATA_STRING = localStorage.getItem('MOCK_DATA');
+    let MOCK_DATA = JSON.parse(MOCK_DATA_STRING);
+    return MOCK_DATA;
+}
+
+function setMockData(MOCK_DATA) {
+    let MOCK_DATA_STRING = JSON.stringify(MOCK_DATA);
+    localStorage.setItem('MOCK_DATA', MOCK_DATA_STRING);
+
+}
+
 function addNewEmployee(employeeData) {
     return new Promise((resolve, reject) => {
+        let MOCK_DATA = getMockData();
+        //also give the new employee points
         MOCK_DATA.employees.push(employeeData);
+        setMockData(MOCK_DATA);
         resolve();
     })
 }
+$("form[name=login-form").submit(function (event) {
+    event.preventDefault();
+    //use the email address to find the logged-in-employee and then list on the screen
+    const employeeEmail = $('input[name=email]');
+    const loggedInEmployeeEmail = employeeEmail.val();
+    //the loggedInEmployeeEmail is correct at this point
+   debugger
+    function findEmployee(employees) {
+        getMockData();
+        debugger
+        for (let currentEmployee = 0; currentEmployee <MOCK_DATA.employees.length; currentEmployee++) {
+            console.log(`${employees[currentEmployee].emailAddress}`);
+        
+            if ((employees[currentEmployee].emailAddress) == (loggedInEmployeeEmail)) {
+                let loggedInEmployee = (`${employees[currentEmployee].firstName} ${employees[currentEmployee].lastName}`)
+debugger
+                $('#js-logged-in-employee').append(`You are logged in as ${loggedInEmployee}`);
+            }
+
+            // else ( error message)
+        }
+    }
+    findEmployee();
+});
+
 
 function getAllEmployees() {
     return new Promise((resolve, reject) => {
+        let MOCK_DATA = getMockData();
         resolve(MOCK_DATA.employees);
     });
 }
@@ -118,16 +153,22 @@ function myApplication() {
 
 
 
-$('#employee-list').click(function (event) {
+$('#js-last-name').click(function (event) {
     console.log("employee has been selected-sending to individual employee page")
     //hide and display correct elements in here when the htmls are combined
+
+
+    $('.employee').append($(event.currentTarget).text());
+    debugger
+
+
 })
 
-$('.js-new-points.button').click(function (event) {
+$('.js-new-points').click(function (event) {
     console.log("sending to addpoints section")
 })
 
-$('.js-add-points-button.button').click(function (event) {
+$('#js-add-points-button').click(function (event) {
     console.log("adding the points");
 
     // for giver and recipient:
