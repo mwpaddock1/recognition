7d0e1d9d0734a63484e3f107157ea84a8de7d09f
Marjorie Paddock <marjorie.paddock@gmail.com>
2018-04-07 13:31:56 -0400

    putting older HTML in a separate folder

--- test/test-server.js
+++ test/test-server.js
@@ -3,7 +3,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../server.js');
-// const expect = chai.expect;
+const expect = chai.expect;
 
 chai.should();
 
