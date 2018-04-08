95936d8aa1be2d5a75aa88ffc861326ed4f74866
Marjorie Paddock <marjorie.paddock@gmail.com>
2018-04-08 16:59:34 -0400

    hiding auth and users to work on CRUD

--- users/models.js
+++ users/models.js
@@ -1,40 +1,40 @@
-'use strict';
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
+// 'use strict';
+// const express = require('express');
+// const bcrypt = require('bcryptjs');
+// const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-const UserSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  firstName: {type: String, default: ''},
-  lastName: {type: String, default: ''}
-});
+// const UserSchema = mongoose.Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   firstName: {type: String, default: ''},
+//   lastName: {type: String, default: ''}
+// });
 
-UserSchema.methods.serialize = function() {
-  return {
-    username: this.username || '',
-    firstName: this.firstName || '',
-    lastName: this.lastName || ''
-  };
-};
+// UserSchema.methods.serialize = function() {
+//   return {
+//     username: this.username || '',
+//     firstName: this.firstName || '',
+//     lastName: this.lastName || ''
+//   };
+// };
 
-UserSchema.methods.validatePassword = function(password) {
-  return bcrypt.compare(password, this.password);
-};
+// UserSchema.methods.validatePassword = function(password) {
+//   return bcrypt.compare(password, this.password);
+// };
 
-UserSchema.statics.hashPassword = function(password) {
-  return bcrypt.hash(password, 10);
-};
+// UserSchema.statics.hashPassword = function(password) {
+//   return bcrypt.hash(password, 10);
+// };
 
-const User = mongoose.model('User', UserSchema);
+// const User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+// module.exports = {User};
