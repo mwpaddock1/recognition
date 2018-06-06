2373bf448d59c0e73722ea1c081d002b0ae9a203
Marjorie Paddock <marjorie.paddock@gmail.com>
2018-06-06 11:12:42 -0400

    removing unnecessary files, updating CSS

--- scratch.js
+++
@@ -1,8 +0,0 @@
-// DATABASE_URL=mongodb://legal:staffer@ds111188.mlab.com:11188/recognitiondb
-
-// TEST_DATABASE_URL=mongodb://testlegal:teststaffer@ds161459.mlab.com:61459/test-recognitiondb
-
-
- 
-
-        
