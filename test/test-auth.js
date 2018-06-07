27a9d19a5309c6f370fef516ca2c712c2aeff4d6
Marjorie Paddock <marjorie.paddock@gmail.com>
2018-06-07 09:20:58 -0400

    Making sure tests are run with the test db

--- test/test-auth.js
+++ test/test-auth.js
@@ -32,7 +32,7 @@ describe('Auth endpoints', function () {
     const pointsRemaining =''
 
     before(function () {
-        return runServer();
+        return runServer(TEST_DATABASE_URL);
     });
 
     after(function () {
